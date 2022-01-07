f7eea10175d07ae9a67fc04d392b2989ff73f55b
karelbecerra <karelbecerra@gmail.com>
2022-01-07 12:33:43 -0500

    Fix claim input

--- webapp/components/ClaimYourName/ClaimYourName.js
+++ webapp/components/ClaimYourName/ClaimYourName.js
@@ -9,7 +9,7 @@ const ClaimYourName = ({}) => {
             <div className="flex flex-row space-x-1 font-mono text-gray-700 text-2xl font-bold leading-6">
               <div className="basis-1/6 h-14 flex items-center justify-end ">supportwithcrypto.com/</div>
               <div className="basis-2/3 h-14 rounded-lg flex items-center justify-start">
-                <input type="text" placeholder="YourName" className="bg-gray-50"></input>
+                <input type="text" placeholder="YourName" className="bg-gray-50 w-full h-14"></input>
               </div>
               <div className="basis-1/6 h-14 rounded-lg flex items-center justify-center bg-fuchsia-700 shadow-lg text-gray-100">Claim</div>
             </div>
