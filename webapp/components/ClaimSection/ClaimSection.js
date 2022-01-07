d4c1983fd0d96653bffca34b2160d19e1e9ab585
karelbecerra <karelbecerra@gmail.com>
2022-01-07 12:31:35 -0500

    Domain change: Support With Crypto

--- webapp/components/ClaimSection/ClaimSection.js
+++ webapp/components/ClaimSection/ClaimSection.js
@@ -13,7 +13,7 @@ const ClaimSection = () => {
 
               <div className="justify-center text-center flex flex-wrap mt-24 mb-4">
                 <div className="w-full md:w-6/12 px-12 md:px-4">
-                  <h2 className="font-semibold text-4xl">Create your <span className="text-fuchsia-700 font-extrabold">Crypto Bean</span> page in just seconds.</h2>
+                  <h2 className="font-semibold text-4xl">Create your <span className="text-fuchsia-700 font-extrabold">Support With Crypto</span> page in just seconds.</h2>
                   <h2 className="">Grow your income and receive payments.</h2>
                 </div>
               </div>
--- webapp/components/ClaimYourName/ClaimYourName.js
+++ webapp/components/ClaimYourName/ClaimYourName.js
@@ -7,7 +7,7 @@ const ClaimYourName = ({}) => {
         <div className="not-prose relative bg-gray-50 rounded-xl overflow-hidden dark:bg-gray-800/25">
           <div className="relative rounded-xl overflow-auto p-4">
             <div className="flex flex-row space-x-1 font-mono text-gray-700 text-2xl font-bold leading-6">
-              <div className="basis-1/6 h-14 flex items-center justify-end ">cryptobean.cc/</div>
+              <div className="basis-1/6 h-14 flex items-center justify-end ">supportwithcrypto.com/</div>
               <div className="basis-2/3 h-14 rounded-lg flex items-center justify-start">
                 <input type="text" placeholder="YourName" className="bg-gray-50"></input>
               </div>
