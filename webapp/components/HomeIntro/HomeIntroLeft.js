f9d30d4c52024039454df68dad92f7ffeca14083
juancolamendy <juanc.olamendy@gmail.com>
2022-01-19 00:51:03 -0500

    test helm3

--- webapp/components/HomeIntro/HomeIntroLeft.js
+++ webapp/components/HomeIntro/HomeIntroLeft.js
@@ -11,6 +11,7 @@ const HomeIntroLeft = () => {
         <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           While other platforms take a fee, we don’t. Qui irure qui lorem cupidatat commodo. Elit sunt amet
           fugiat veniam occaecat fugiat aliqua.
+          Support with crypto test
         </p>
         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div className="rounded-md shadow">
