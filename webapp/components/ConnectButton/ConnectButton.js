d173668045c120c0ece10d7a41428141fc3b747e
karelbecerra <karelbecerra@gmail.com>
2022-02-02 13:41:56 -0500

    link from home to micropage

--- webapp/components/ConnectButton/ConnectButton.js
+++ webapp/components/ConnectButton/ConnectButton.js
@@ -80,7 +80,7 @@ const ConnectButton = ({label}) => {
   return (
   <div className="w-full text-sm leading-4 font-medium text-green-900">
     { account ? (
-      <div className="flex items-center justify-center">
+      <a type="button" href={'/' + account} className="flex items-center justify-center">
         <span>{`${formatCurrency(etherBalance)} ${symbol}`} -</span>
         <svg width="24" height="24" stroke="currentColor" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1 text-cyan-500">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M19.25 8.25V17.25C19.25 18.3546 18.3546 19.25 17.25 19.25H6.75C5.64543 19.25 4.75 18.3546 4.75 17.25V6.75"></path>
@@ -88,7 +88,7 @@ const ConnectButton = ({label}) => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.25 8.25H6.5C5.5335 8.25 4.75 7.4665 4.75 6.5C4.75 5.5335 5.5335 4.75 6.5 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V8.25ZM17.25 8.25H19.25"></path>
         </svg>
         <span>{`${formatAccount(account)}`}</span>
-      </div>
+      </a>
     ) : (
       <button
         type="button" 
