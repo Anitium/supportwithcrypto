c2d3ee4fdd5440cd89e327267f5203bed6e6a341
karelbecerra <karelbecerra@gmail.com>
2022-02-03 19:40:53 -0500

    team initial layout

--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -1,12 +1,16 @@
 import HomeIntro from '../components/home/HomeIntro';
+import HomeTeam from '../components/home/HomeTeam';
 import { DefaultLayout } from '../layout';
 
 const Index = () => {
   return (
     <>
-    <content className="container mx-auto flex justify-center my-10">
-      <section className='flex flex-row w-full justify-center space-x-10 max-w-6xl'>
+    <content className="container mx-auto flex flex-col my-10 max-w-6xl">
+      <section className='flex flex-row w-full justify-center space-x-10'>
         <HomeIntro />
+      </section>
+      <section className='flex flex-row w-full justify-center items-center'>
+        <HomeTeam />
       </section>
     </content>
     </>
