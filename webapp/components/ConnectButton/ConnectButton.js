5ae646a3edd728522367ae715af3f9e50deef2cc
karelbecerra <karelbecerra@gmail.com>
2022-02-04 09:21:02 -0500

    add logut icon

--- webapp/components/ConnectButton/ConnectButton.js
+++ webapp/components/ConnectButton/ConnectButton.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
+import { LogoutIcon } from '@heroicons/react/outline'
 import { useEthers, useEtherBalance } from "@usedapp/core";
 import { ethers } from 'ethers';
 import Web3Modal from "web3modal";
@@ -104,9 +105,7 @@ const ConnectButton = ({label}) => {
           </div>
           <div>
             <a href="#" onClick={handleDisconnect}>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-              </svg> 
+              <LogoutIcon className="h-4 w-4" />
             </a>
           </div>
         </div>
