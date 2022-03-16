064101e45b40e8e4598b4a7bc1b885ba5ccaaf16
karelbecerra <karelbecerra@gmail.com>
2022-03-16 14:52:10 -0400

    add external blog

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,3 +1,21 @@
+const { BLOG_URL } = process.env
+
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/crypto',
+        destination: `${BLOG_URL}/crypto`,
+      },
+      {
+        source: '/crypto/:path*',
+        destination: `${BLOG_URL}/crypto/:path*`,
+      },
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+    ]
+  },
 }
